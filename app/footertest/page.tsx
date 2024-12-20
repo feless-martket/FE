57f4baf1b8119ba5817510aca922efc0b5942c88
SangHyun-e <66759329+SangHyun-e@users.noreply.github.com>
2024-12-20 14:10:38 +0900

    feat: LoginPage, Header, Footer

---
+++ app/footertest/page.tsx
@@ -0,0 +1,9 @@
+import { Footer } from "@/components/layout/footer";
+
+export default function Page() {
+  return (
+    <div className="flex h-screen w-full justify-center bg-gray-100">
+      <Footer></Footer>
+    </div>
+  );
+}
---
+++ components/layout/header.tsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+interface HeaderProps {
+  title: string;
+  showDivider?: boolean;
+  dividerClassName?: string;
+  closeButton?: ReactNode;
+  className?: string;
+}
+
+export function Header({
+  title,
+  showDivider = true,
+  dividerClassName = "border-b border-gray-200",
+  closeButton,
+  className = " ",
+}: HeaderProps) {
+  return (
+    <div className={`relative mb-4 ${showDivider ? "pb-4" : ""} ${className}`}>
+      <div className="text-center text-lg font-medium">{title}</div>
+      {closeButton || (
+        <Link
+          href="/"
+          className="absolute left-0 top-1 flex size-5 items-center justify-center"
+        >
+          <span className="text-xl">&times;</span>
+        </Link>
+      )}
+      {showDivider && (
+        <div className={`absolute inset-x-0 bottom-0 ${dividerClassName}`} />
+      )}
+    </div>
+  );
+}
