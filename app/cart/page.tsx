8cdc623ff2562d8b9b7f2dabcd6657858f50e09a
yoonhyojin <dbsgywls9855@gmail.com>
2025-01-13 13:52:49 +0900

    bg-color 통일 및 footer 여백 설정

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -3,8 +3,8 @@ import { Footer } from "@/components/layout/footer";
 
 export default function CategoryPage() {
   return (
-    <div className="flex flex-col justify-center bg-gray-100 pb-[52px]">
-      <div className="mx-auto max-w-[360px] bg-white">
+    <div className="flex h-screen w-full justify-center bg-gray-100 pb-[52px]">
+      <div className="flex w-[360px] flex-col bg-white">
         <ShoppingCart />
       </div>
       <Footer />
