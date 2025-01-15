0395b9ab10ba860c94df2cfa11d52aa34d1cdabd
SangHyun-e <66759329+SangHyun-e@users.noreply.github.com>
2025-01-15 17:48:01 +0900

    fix: 아이디 찾기 헤더에선 'X' 버튼 대신 '<' 버튼 으로 변경, landing page가 아닌 login page로 라우팅

--- app/find-id/page.tsx
+++ app/find-id/page.tsx
@@ -2,11 +2,24 @@
 import { Header } from "@/components/layout/header";
 import { PageLayout } from "@/components/layout/pagelayout";
 import { FindIdForm } from "@/feature/find-id/find-id-form";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 
 export default function FindIdPage() {
   return (
     <PageLayout>
-      <Header title="아이디 찾기" showDivider={false} />
+      <Header
+        title="아이디 찾기"
+        showDivider={false}
+        closeButton={
+          <Link
+            href="/login"
+            className="absolute left-4 top-1/2 -translate-y-1/2"
+          >
+            <ChevronLeft className="size-6" />
+          </Link>
+        }
+      />
       <FindIdForm />
     </PageLayout>
   );
--- app/find-id/success/page.tsx
+++ app/find-id/success/page.tsx
@@ -3,6 +3,8 @@
 import { Header } from "@/components/layout/header";
 import { PageLayout } from "@/components/layout/pagelayout";
 import { FindIdSuccess } from "@/feature/find-id/find-id-success";
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 
 export default function FindIdSuccessPage() {
@@ -12,7 +14,18 @@ export default function FindIdSuccessPage() {
 
   return (
     <PageLayout>
-      <Header title="아이디 찾기" showDivider={false} />
+      <Header
+        title="아이디 찾기"
+        showDivider={false}
+        closeButton={
+          <Link
+            href="/login"
+            className="absolute left-4 top-1/2 -translate-y-1/2"
+          >
+            <ChevronLeft className="h-6 w-6" />
+          </Link>
+        }
+      />
       <FindIdSuccess username={username} />
     </PageLayout>
   );
